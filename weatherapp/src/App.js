a72e1e531502f39e2bcf84788723584fe0f55050
Richie1136 <Richiemajor1@gmail.com>
2021-08-02 14:11:27 -0400

    Debugging error

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -15,13 +15,12 @@ function App() {
 
   const search = (event) => {
     if (event.key === "Enter") {
-      fetch(`${Api.base} weather?q=${query}&units=metric&APPID=${Api.key}`)
+      fetch(`${Api.base}weather?q=${query}&units=metric&APPID=${Api.key}`)
         .then(res => res.json())
         .then(result => {
-          setQuery('')
           setWeather(result)
+          setQuery('')
           console.log(result)
-          console.log(weather)
         })
     }
   }
@@ -38,7 +37,12 @@ function App() {
     return `${day}  ${month} ${date} ${year}`
   }
   return (
-    <div className="App">
+    <div className={
+      (typeof weather.main !== 'undefined')
+        ? ((weather.main.temp > 50)
+          ? 'App warm'
+          : 'App')
+        : 'App'}>
       <main>
         <div className='search-box'>
           <input type='text'
@@ -49,20 +53,22 @@ function App() {
             onKeyPress={search}
           />
         </div>
-        <div className='location-box'>
-          <div className='location'>Long Island, NY, US</div>
-          <div className='date'>{dateBuilder(new Date())}</div>
-        </div>
-        <div className='weather-box'>
-          <div className='temp'>
-            70°F
+        {(typeof weather.main !== 'undefined') ? (
+          <div>
+            <div className='location-box'>
+              <div className='location'>{weather.name}, {weather.sys.country}</div>
+              <div className='date'>{dateBuilder(new Date())}</div>
+            </div>
+            <div className='weather-box'>
+              <div className='temp'>
+                {Math.random(weather.main.temp)}°F
+              </div>
+              <div className='weather'>{weather.weather[0].main}</div>
+            </div>
           </div>
-          <div className='weather'>
-            Sunny
-          </div>
-        </div>
+        ) : ('')}
       </main>
-    </div>
+    </div >
   );
 }
 
