2bcf2266f14fef4aa4be305063222602384fe3ee
Richie1136 <Richiemajor1@gmail.com>
2021-08-02 11:59:42 -0400

    Adding some functionality to the weather app

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import REACT_APP_API_URL from './api/key'
 import REACT_APP_API_KEY from './api/key'
+import { useState } from 'react'
 
 const Api = {
   key: REACT_APP_API_KEY,
@@ -9,6 +10,23 @@ const Api = {
 }
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [weather, setWeather] = useState({})
+
+  const search = (event) => {
+    if (event.key === "Enter") {
+      fetch(`${Api.base} weather?q=${query}&units=metric&APPID=${Api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setQuery('')
+          setWeather(result)
+          console.log(result)
+          console.log(weather)
+        })
+    }
+  }
+
+
   const dateBuilder = (d) => {
     let months = ['January', "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -26,6 +44,9 @@ function App() {
           <input type='text'
             className='search-bar'
             placeholder='search'
+            onChange={event => setQuery(event.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
         <div className='location-box'>
