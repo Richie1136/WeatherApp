75d93ad24ec705d05a727462f136a36893c697e6
Richie1136 <Richiemajor1@gmail.com>
2021-08-02 10:36:29 -0400

    Setting up the files

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import REACT_APP_API_URL from './api/key'
+import REACT_APP_API_KEY from './api/key'
+
+const Api = {
+  key: REACT_APP_API_KEY,
+  base: REACT_APP_API_URL
+}
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <main>
+        <div className='search-box'>
+          <input type='text'
+            className='search-bar'
+            placeholder='search'
+          />
+        </div>
+      </main>
     </div>
   );
 }
